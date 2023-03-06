fa8df9a44e5d3b14c9206e1a73ad25405df63f42
Koblik_Oleksandr <126065802+OleksandrKoblik@users.noreply.github.com>
2023-03-06 12:24:43 +0200

    add all

---
+++ website-business-card/src/app/pages/detail-info/detail-info.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DetailInfoComponent } from './detail-info.component';
+
+
+
+@NgModule({
+  declarations: [
+    DetailInfoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DetailInfoModule { }
---
+++ website-business-card/src/app/pages/terms-of-use/terms-of-use.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TermsOfUseComponent } from './terms-of-use.component';
+
+
+
+@NgModule({
+  declarations: [
+    TermsOfUseComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TermsOfUseModule { }
